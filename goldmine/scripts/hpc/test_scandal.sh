70d62175cc23406a82d2095d55fac8d1679ca429
Johann <mail@johannbrehmer.de>
2018-07-10 14:24:52 -0400

    Cluster jobs running

--- goldmine/scripts/hpc/test_scandal.sh
+++ goldmine/scripts/hpc/test_scandal.sh
@@ -21,14 +21,3 @@ cd /home/jb6504/goldmine/goldmine
 ./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 20000 --classifiertest
 ./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 50000 --classifiertest
 ./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --classifiertest
-
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 100 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 200 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 500 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 1000 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 2000 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 5000 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 10000 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 20000 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 50000 --classifiertest --singletheta
-./test.py epidemiology2d scandal -i ${SLURM_ARRAY_TASK_ID} --classifiertest --singletheta
