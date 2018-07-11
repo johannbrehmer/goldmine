9ae46a2c72cae199c7c341b8189a1781872c5626
Johann Brehmer <mail@johannbrehmer.de>
2018-07-11 12:29:59 -0400

    Performance on one epidemiology2d run

--- goldmine/scripts/local/train_histo.sh
+++ goldmine/scripts/local/train_histo.sh
@@ -2,13 +2,13 @@
 
 cd /Users/johannbrehmer/work/projects/scandal/goldmine/goldmine
 
-./train.py epidemiology2d histogram --samplesize 100 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 200 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 500 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 1000 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 2000 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 5000 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 10000 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 20000 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 50000 --singletheta --fillemptybins --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --singletheta --fillemptybins --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 100 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 200 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 500 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 1000 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 2000 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 5000 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 10000 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 20000 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 50000 --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --singletheta  --xhistos1d --xbins 10
