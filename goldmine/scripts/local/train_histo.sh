01edfb933041f5db8fdc396f05cf5edd4b804841
Johann Brehmer <mail@johannbrehmer.de>
2018-07-11 17:22:20 -0400

    Histogram debugging, result plots

--- goldmine/scripts/local/train_histo.sh
+++ goldmine/scripts/local/train_histo.sh
@@ -2,13 +2,13 @@
 
 cd /Users/johannbrehmer/work/projects/scandal/goldmine/goldmine
 
-./train.py epidemiology2d histogram --samplesize 100 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 200 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 500 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 1000 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 2000 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 5000 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 10000 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 20000 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --samplesize 50000 --singletheta  --xhistos1d --xbins 10
-./train.py epidemiology2d histogram --singletheta  --xhistos1d --xbins 10
+./train.py epidemiology2d histogram --samplesize 100 --singletheta  --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 200 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 500 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 1000 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 2000 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 5000 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 10000 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 20000 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --samplesize 50000 --singletheta   --xbins 20 --xhistos1d
+./train.py epidemiology2d histogram --singletheta   --xbins 20 --xhistos1d
