75db1c0ecf7a01bcaa463b4bf905e5b3c0dd7c46
Johann <mail@johannbrehmer.de>
2018-09-11 08:45:02 -0400

    Changed cluster script

--- goldmine/scripts/hpc/simulate_lv.sh
+++ goldmine/scripts/hpc/simulate_lv.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
-#SBATCH --time=4-00:00:00
-# #SBATCH --gres=gpu:1
+#SBATCH --time=5-00:00:00
 
+source activate goldmine
 cd /home/jb6504/goldmine/goldmine/
 
 ./simulate.py --nthetas 10000 --nsamples 100 --noratio lotkavolterra train
