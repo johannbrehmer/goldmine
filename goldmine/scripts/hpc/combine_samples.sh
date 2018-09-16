958438a1d79c4a2e1a51818b99d5ddf2eb954308
Johann <mail@johannbrehmer.de>
2018-09-16 10:18:54 -0400

    Sample combination seems to work, updated HPC script

--- goldmine/scripts/hpc/combine_samples.sh
+++ goldmine/scripts/hpc/combine_samples.sh
@@ -4,10 +4,11 @@
 #SBATCH --output=combine_samples.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=256GB
+#SBATCH --mem=62GB
 #SBATCH --time=1-00:00:00
 
 source activate goldmine
 cd /home/jb6504/goldmine/goldmine/
 
-./combine_samples.py --regex lotkavolterra train_combined train\d+
+./combine_samples.py --regex lotkavolterra train "train\d+"
+./combine_samples.py --regex lotkavolterra test_singletheta "test\d+_singletheta"
