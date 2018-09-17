61bddc8798e1c33af27ff6a577de53f2a118029d
Johann Brehmer <mail@johannbrehmer.de>
2018-09-17 10:19:07 +0100

    HPC scripts to train LV models

---
+++ goldmine/scripts/hpc/train_lv_maf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_maf
+#SBATCH --output=train_maf_%a.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=3-00:00:00
+#SBATCH --gres=gpu:1
+
+source activate goldmine
+cd /home/jb6504/goldmine/goldmine
+
+./train.py epidemiology2d maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 1000
+./train.py epidemiology2d maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 10000
+./train.py epidemiology2d maf -i ${SLURM_ARRAY_TASK_ID}
