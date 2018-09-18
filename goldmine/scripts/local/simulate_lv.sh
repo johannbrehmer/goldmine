d28f0312aa531b7bf5218df5b7e6fe2fa73e3d9e
Johann Brehmer <mail@johannbrehmer.de>
2018-09-18 15:35:25 +0100

    More local experiments

---
+++ goldmine/scripts/local/simulate_lv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /Users/johannbrehmer/work/projects/scandal/goldmine/goldmine
+
+./simulate.py --singletheta --nsamples 100 --noratio --debug lotkavolterra small
+# ./simulate.py --nthetas 100 --nsamples 1 --noratio --debug lotkavolterra small
