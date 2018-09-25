24125836bda469b70ee0430d05846a2edb9078f1
Johann Brehmer <mail@johannbrehmer.de>
2018-09-25 21:08:21 +0100

    Another change to HPC scripts to avoid overwriting old results

--- goldmine/scripts/hpc/lotkavolterra/train_lv_maf.sh
+++ goldmine/scripts/hpc/lotkavolterra/train_lv_maf.sh
@@ -11,11 +11,11 @@
 source activate goldmine
 cd /scratch/jb6504/goldmine/goldmine
 
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 1000 --trainsample train_focus
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 2000 --trainsample train_focus
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 5000 --trainsample train_focus
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 10000 --trainsample train_focus
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 20000 --trainsample train_focus
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 50000 --trainsample train_focus
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 100000 --trainsample train_focus
-./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 200000 --trainsample train_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 1000 --trainsample train_focus --modellabel model_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 2000 --trainsample train_focus --modellabel model_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 5000 --trainsample train_focus --modellabel model_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 10000 --trainsample train_focus --modellabel model_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 20000 --trainsample train_focus --modellabel model_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 50000 --trainsample train_focus --modellabel model_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 100000 --trainsample train_focus --modellabel model_focus
+./train.py lotkavolterra maf -i ${SLURM_ARRAY_TASK_ID} --samplesize 200000 --trainsample train_focus --modellabel model_focus
