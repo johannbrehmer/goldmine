174f27f054e2e711764076ae281f1b5fbcc67956
Johann <mail@johannbrehmer.de>
2018-09-28 10:14:20 -0400

    Cleaned up HPC scripts

--- goldmine/scripts/hpc/epidemiology/submit_test.sh
+++ goldmine/scripts/hpc/epidemiology/submit_test.sh
@@ -4,4 +4,4 @@ cd /scratch/jb6504/goldmine/goldmine/scripts/hpc/epidemiology
 
 sbatch --array=0-4 test_maf.sh
 sbatch --array=0-4 test_scandal.sh
-sbatch --array=0-4 test_rascandal.sh
+# sbatch --array=0-4 test_rascandal.sh
---
+++ goldmine/scripts/hpc/lotkavolterra/test_scandal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_scandal
+#SBATCH --output=log_test_scandal_%a.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=1-00:00:00
+# #SBATCH --gres=gpu:1
+
+source activate goldmine
+cd /scratch/jb6504/goldmine/goldmine
+
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 1000 --densitygrid --ratiogrid --density --score  --testsample test_focus --modellabel model_focus
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 2000 --densitygrid --ratiogrid --density --score --testsample test_focus --modellabel model_focus
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 5000 --densitygrid --ratiogrid --density --score --testsample test_focus --modellabel model_focus
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 10000 --densitygrid --ratiogrid --density --score --testsample test_focus --modellabel model_focus
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 20000 --densitygrid --ratiogrid --density --score --testsample test_focus --modellabel model_focus
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 50000 --densitygrid --ratiogrid --density --score --testsample test_focus --modellabel model_focus
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 100000 --densitygrid --ratiogrid --density --score --testsample test_focus --modellabel model_focus
+./test.py lotkavolterra scandal -i ${SLURM_ARRAY_TASK_ID} --samplesize 200000 --densitygrid --ratiogrid --density --score --testsample test_focus --modellabel model_focus
