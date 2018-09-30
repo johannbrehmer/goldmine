29315e1e0ee227cc6f242cc63a20eb6019dee392
Johann Brehmer <mail@johannbrehmer.de>
2018-09-30 19:52:03 +0100

    Test HPC scripts for ratio-based methods

--- goldmine/scripts/hpc/lotkavolterra/submit_test.sh
+++ goldmine/scripts/hpc/lotkavolterra/submit_test.sh
@@ -2,7 +2,12 @@
 
 cd /scratch/jb6504/goldmine/goldmine/scripts/hpc/lotkavolterra
 
-sbatch --array=0-4 test_maf.sh
-sbatch --array=0-4 test_scandal.sh
-sbatch --array=0-4 test_rascandal.sh
+# sbatch --array=0-4 test_maf.sh
+# sbatch --array=0-4 test_scandal.sh
+# sbatch --array=0-4 test_rascandal.sh
 # sbatch --array=0-4 test_scandalcv.sh
+
+sbatch --array=0-4 test_carl.sh
+sbatch --array=0-4 test_rolr.sh
+sbatch --array=0-4 test_cascal.sh
+sbatch --array=0-4 test_rascal.sh
