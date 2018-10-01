baa12486d5737fc01fa603bd0a83aa7dd5523e2b
Johann Brehmer <mail@johannbrehmer.de>
2018-10-01 15:57:59 +0100

    Local model analysis for zoomed-in region

--- goldmine/scripts/local/lotkavolterra/simulate_lv_zoompilot.sh
+++ goldmine/scripts/local/lotkavolterra/simulate_lv_zoompilot.sh
@@ -2,4 +2,4 @@
 
 cd /Users/johannbrehmer/work/projects/scandal/goldmine/goldmine
 
-./simulate.py --nthetas 1000 --nsamples 1 --noscore --noratio lotkavolterra zoompilot
+./simulate.py --nthetas 1000 --nsamples 1 lotkavolterra zoompilot
