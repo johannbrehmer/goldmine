67d86b84d22aad3712183d5b9604d7466d7e53c7
Johann <mail@johannbrehmer.de>
2018-10-03 14:05:21 -0400

    Changes to HPC starter scripts

--- goldmine/scripts/hpc/lotkavolterra/submit_train.sh
+++ goldmine/scripts/hpc/lotkavolterra/submit_train.sh
@@ -10,11 +10,12 @@ cd /scratch/jb6504/goldmine/goldmine/scripts/hpc/lotkavolterra
 # sbatch --array=0-9 train_scandalmog.sh
 # sbatch --array=0-9 train_rascandalmog.sh
 
-sbatch --array=5-9 train_rascal.sh
-sbatch --array=5-9 train_cascal.sh
-sbatch --array=5-9 train_rolr.sh
-sbatch --array=5-9 train_carl.sh
+# sbatch --array=0-9 train_rascal.sh
+# sbatch --array=0-9 train_cascal.sh
+# sbatch --array=0-9 train_rolr.sh
+# sbatch --array=0-9 train_carl.sh
 
-sbatch --array=0-0 train_mafmog_singletheta.sh
 # sbatch --array=0-9 train_scandal_largealpha.sh
 # sbatch --array=0-9 train_scandalmog_largealpha.sh
+
+sbatch --array=0-9 train_groundtruth.sh
