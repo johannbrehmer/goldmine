3bc421fc0e19f0f8357e4f3498d6884e8dfce8b0
Johann Brehmer <mail@johannbrehmer.de>
2018-11-12 10:24:33 -0500

    Fixed FlowCheckpointScoreModel

--- goldmine/ml/models/checkpoint.py
+++ goldmine/ml/models/checkpoint.py
@@ -17,15 +17,17 @@ class FlowCheckpointScoreModel(nn.Module):
         logging.debug("  theta = %s", theta)
         logging.debug("  z_checkpoints = %s", z_checkpoints)
 
-        t_checkpoints = []
-        for z_initial, z_final in zip(z_checkpoints[:, -1], z_checkpoints[:, 1:]):
-            logging.debug("  z step: %s -> %s", z_initial, z_final)
-            t_checkpoints.append(
-                self.step_model.forward(z_initial, z_final, theta).unsqueeze(1)
-            )
-            logging.debug("  that for step: %s", t_checkpoints[-1])
-        t_checkpoints = torch.cat(t_checkpoints, 1)  # Shape (n_batch, n_checkpoints, n_params)
-        logging.debug("  Concatenated that: %s", t_checkpoints)
+        n_batch, n_steps, n_latent = z_checkpoints.size()
+
+        z_initial = z_checkpoints[:,:-1,:].view(-1,n_latent)
+        z_final = z_checkpoints[:,1:,:].view(-1,n_latent)
+
+        logging.debug("  z_initial = %s", z_initial)
+        logging.debug("  z_final = %s", z_final)
+
+        t_checkpoints = self.step_model.forward(z_initial, z_final, theta)
+        t_checkpoints = t_checkpoints.view(n_batch, n_steps, n_latent)
+        logging.debug("  that: %s", t_checkpoints)
 
         return t_checkpoints
 
