b26f2552a78fef19aa977da98840b1aa77b3733f
Johann Brehmer <mail@johannbrehmer.de>
2018-11-12 15:20:55 -0500

    Bug fixes and more debugging output in FlowCheckpointScoreModel

--- goldmine/ml/models/checkpoint.py
+++ goldmine/ml/models/checkpoint.py
@@ -1,6 +1,5 @@
 import logging
 import torch.nn as nn
-import torch
 
 
 class FlowCheckpointScoreModel(nn.Module):
@@ -14,20 +13,28 @@ class FlowCheckpointScoreModel(nn.Module):
     def forward_checkpoints(self, theta, z_checkpoints):
         logging.debug("Evaluating forward_checkpoints():")
 
-        logging.debug("  theta = %s", theta)
-        logging.debug("  z_checkpoints = %s", z_checkpoints)
+        logging.debug("  theta: %s, %s", theta.size(), theta)
+        logging.debug("  z_checkpoints: %s, %s", z_checkpoints.size(), z_checkpoints)
 
         n_batch, n_steps, n_latent = z_checkpoints.size()
 
         z_initial = z_checkpoints[:,:-1,:].contiguous().view(-1,n_latent)
         z_final = z_checkpoints[:,1:,:].contiguous().view(-1,n_latent)
 
-        logging.debug("  z_initial = %s", z_initial)
-        logging.debug("  z_final = %s", z_final)
+        theta_step = theta.clone()
+        theta_step.unsqeeze(1)  # (n_batch, 1, n_parameters)
+        theta_step = theta_step.repeat(1,n_steps,1)
+        theta_step = theta_step.contiguous().view(-1,n_latent)
+
+        logging.debug("  theta_step: %s, %s", theta_step.size(), theta_step)
+        logging.debug("  z_initial: %s, %s", z_initial.size(), z_initial)
+        logging.debug("  z_final: %s, %s", z_final.size(), z_final)
 
         t_checkpoints = self.step_model.forward(z_initial, z_final, theta)
+        logging.debug("  that: %s, %s", t_checkpoints.size(), t_checkpoints)
+
         t_checkpoints = t_checkpoints.view(n_batch, n_steps, n_latent)
-        logging.debug("  that: %s", t_checkpoints)
+        logging.debug("  that: %s, %s", t_checkpoints.size(), t_checkpoints)
 
         return t_checkpoints
 
