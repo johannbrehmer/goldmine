84ff2d2aa4fecafec1385e79c0c0d947e0958e84
Johann <mail@johannbrehmer.de>
2019-01-24 16:04:01 -0500

    Changed HPC script

--- goldmine/scripts/hpc/lotkavolterra/combine_samples.sh
+++ goldmine/scripts/hpc/lotkavolterra/combine_samples.sh
@@ -12,5 +12,5 @@ source activate goldmine
 cd /scratch/jb6504/goldmine/goldmine/
 
 ./combine_samples.py --regex lotkavolterra train_zoom "train_zoom_\d+"
-# ./combine_samples.py --regex lotkavolterra test_zoom_singletheta "test_zoom_\d+_singletheta"
-# ./combine_samples.py --regex lotkavolterra test_zoom "test_zoom_\d+"
+./combine_samples.py --regex lotkavolterra test_zoom_singletheta "test_zoom_\d+_singletheta"
+./combine_samples.py --regex lotkavolterra test_zoom "test_zoom_\d+"
