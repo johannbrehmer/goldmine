cabf2238c44fdb6e8027ec6b038b31e49b44a41d
Johann <mail@johannbrehmer.de>
2019-01-25 11:50:33 -0500

    Changes to HPC scripts

--- goldmine/scripts/hpc/lotkavolterra/submit_train.sh
+++ goldmine/scripts/hpc/lotkavolterra/submit_train.sh
@@ -2,12 +2,14 @@
 
 cd /scratch/jb6504/goldmine/goldmine/scripts/hpc/lotkavolterra
 
-sbatch --array=0-9 train_mafmog.sh
-sbatch --array=0-9 train_scandalmog.sh
-sbatch --array=0-9 train_scandalmog_largealpha.sh
-sbatch --array=0-9 train_scandalmog_smallalpha.sh
+sbatch --array=0-9 train_checkpoint_scandalmog.sh
 
-sbatch --array=0-9 train_rascal.sh
-sbatch --array=0-9 train_cascal.sh
-sbatch --array=0-9 train_rolr.sh
-sbatch --array=0-9 train_carl.sh
+# sbatch --array=0-9 train_mafmog.sh
+# sbatch --array=0-9 train_scandalmog.sh
+# sbatch --array=0-9 train_scandalmog_largealpha.sh
+# sbatch --array=0-9 train_scandalmog_smallalpha.sh
+
+# sbatch --array=0-9 train_rascal.sh
+# sbatch --array=0-9 train_cascal.sh
+# sbatch --array=0-9 train_rolr.sh
+# sbatch --array=0-9 train_carl.sh
